d7c348ac47b0c6ea7ece9aafbfabca75633a5386
MarcoGuglielmino <marco.guglielmino@musement.com>
2023-07-06 21:17:14 +0200

    feat: convert app to single file component too

--- tour-degli-eroi/src/app/app.component.ts
+++ tour-degli-eroi/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <h1>{{ title }}</h1>
+    <app-eroi></app-eroi>
+  `,
+  styles: [``],
 })
 export class AppComponent {
   title = 'tour-degli-eroi';
